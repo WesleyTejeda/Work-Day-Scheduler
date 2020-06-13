6b5d2fac4bd76d275d622527f94b1354dc2d915a
Wesley Tejeda <xuene07@gmail.com>
2020-06-12 21:20:48 -0400

    Updated index HTML to put timeblock on parent div, Updated JS file with function to add current class to current hour timeblock

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 //Time
+var currentHour = "";
+var timeFormat = "";
 function timer(){
     stopwatch = setInterval(function(){
-        var currentHour = moment().format('h:a');
-        var timeFormat = moment().format('dddd, MMMM Do YYYY');
+        currentHour = moment().format('hA');
+        timeFormat = moment().format('dddd, MMMM Do YYYY');
         $("#currentDay").text(timeFormat);
-        console.log(time);
+        checkPast();
+        checkPresent();
+        checkFuture();
     }, 1000);
 }
 timer();
 //Create function to add past class to timeblock if time > timeblock hour
 function checkPast(){
-
+    console.log(currentHour);
+    var tempHour = currentHour.split(":");
+    $(".timeblock").each(function(){
+        if($(this).children("div").html() == currentHour){
+            $(this).children("textarea").addClass("past");
+        }
+    });
 }
 //Create function to add present class to timeblock if time = timeblock hour
 function checkPresent(){
-
+    //Looks at every timeblock element
+    $(".timeblock").each(function(){
+        //Targetting the div child in timeblock to get the hour of the block and compare it to current time hour
+        //The format for the hours at this stage is HPM == HPM i.e 5PM == 5PM
+        if($(this).children("div").html() == currentHour){
+            //If the hours are the same then  present class gets added to textarea child element  to get the textbox color
+            $(this).children("textarea").addClass("present");
+        }
+    });
 }
-//Create function to add future class to timeblock if time < timeblock hour
+// //Create function to add future class to timeblock if time < timeblock hour
 function checkFuture(){
-
+    $(".timeblock").each(function(){
+        if($(this).children("div").html() == currentHour){
+            $(this).children("textarea").addClass("future");
+        }
+    });
 }
