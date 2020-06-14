6e96405f8f8f9f54492fdd934f32d4d0b993e697
Wesley Tejeda <xuene07@gmail.com>
2020-06-14 18:03:26 -0400

    Updated all files: Updated index HTML to only include 9-5 format, Updated JS to condense functions and to fine tune sections and update comments, Updated README detailing approach and features

--- README.md
+++ README.md
@@ -4,3 +4,33 @@ By: ```Wesley Tejeda```
 Deployed link: [https://wesleytejeda.github.io/Work-Day-Scheduler]
 
 My GitHub : [https://github.com/wesleytejeda/Work-Day-Scheduler]
+
+## General
+In this repository I contained files for a website that displays a schedule for the current day. This page contains timeblocks from 9AM-6PM and each timeblock is marked with their appropriate class to display whether the block is in the past, present, or future. The user can write a message in the textbox and when they save and refresh the message will still be there.
+
+## My Approach in Detail
+*Before I made my ```script.js``` file I decided that my timeblock was going to be the parent div of the timeblock information. This was done so that I could access the children simpler; straight from the parent rather than assigning the timeblock class to the div that contains the time and having to refer back to the parent and then go back to either the save button or the textarea.*
+
+First I created a timer to display our current day, update our time variables, and execute our functions.
+
+Next we move onto assigning past, present, and future classes to our timeblock. I decided to use a more complex method using logic cases in determining the tense instead of using military time. With this approach we take the integer of our time and split it from the AM or PM value and house each part in a seperate variable. From those 2 variables we then have a series of if-else statements that compare each timeblock to the current time and assign the proper class.
+
+Then, in order to save the users message I used a for each function for all save buttons. On clicking a save button, the text and timeblock hour for that specific field is pushed into our array as an object with properties of message and timeblock.
+
+Lastly, similar to our save feature, our ```updateMessages()``` function loops through our local storage. We extract the message and the timeblock hour and use it in a for each function for every timeblock. Inside the function we compare every timeblock or our extracted timeblock hour. If the hours match then our message is displayed on that timeblock's textarea tag.
+
+### Friendy Feature
+I decided to include a function to clear all text from textboxes. This function also pushes an empty array into our local storage effectively clearing previous messages stored.
+```
+$("#clear").on("click", function(){
+    //Empty messageArray
+    messageArray = [];
+    //We must pass the string "[]" to simulate an empty array
+    localStorage.setItem("messages","[]");
+    //To clear every text field we go through each and replace the text with a space
+    $("textarea").each(function(){
+        $(this).html(" ");
+    });
+});
+```
+This was a "nice to have" feature for the user instead of having to manually clear all text boxes. 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Time
 var currentHour = "";
 var timeFormat = "";
-//Create an array to push objects to local storage w/ an index
+//Create an array to push objects to local storage, if theres something in storage we retrieve first
 var messageArray = JSON.parse(localStorage.getItem("messages")) || [];
 //Timer to constantly update our time as well as call our function periodically
 function timer(){
@@ -10,22 +10,19 @@ function timer(){
         timeFormat = moment().format('dddd, MMMM Do YYYY');
         $("#currentDay").text(timeFormat);
         //We always want to update the timeblocks so we call the functions inside of our timer
-        checkPast();
-        checkPresent();
-        checkFuture();
+        assignPastPresentFuture();
         updateMessages();
     }, 1000);
 }
 timer();
 //Create function to add past class to timeblock if current time > timeblock hour
-function checkPast(){
+function assignPastPresentFuture(){
     $(".timeblock").each(function(){
         //tempDivHour takes the value of timeblock
         var tempDivHour = $(this).children("div").html();
         var tempDivPMorAM = "";
         var tempCurrentPMorAM = "";
         var tempHour= currentHour;
-        /*Sloppy method below will fix with a better method, it's working but not effecient */
         /**Assigning PM or AM values taken from div child's timeblock hour.
         ///When timeblock's string is 3 characters long i.e 9AM,5PM,...--*/
         if(tempDivHour.length == 3){
@@ -52,6 +49,7 @@ function checkPast(){
         //Turning our number strings into integers to compare
         tempDivHour = parseInt(tempDivHour);
         tempHour = parseInt(tempHour);
+    /**PAST */
         //If timeblock hour is less than current hour and they have same type of PM or AM
         /*We have to ignore timeblock 12PM because if current time is 1PM, 
         //then 12PM is technically larger but isn't logical */
@@ -75,56 +73,14 @@ function checkPast(){
             //If the timeblock is in the past then we overwrite these classes to textarea child element to get the textbox color
             $(this).children("textarea").attr("class","col-8 col-lg-8 col-md-8 col-sm-8 textarea past");
         }
-
-    });
-};
-//Create function to add present class to timeblock if time = timeblock hour
-function checkPresent(){
-    //Looks at every timeblock element
-    $(".timeblock").each(function(){
+    /**PRESENT */
         //Targetting the div child in timeblock to get the hour of the block and compare it to current time hour
         //The format for the hours at this stage is HPM == HPM i.e 5PM == 5PM
         if($(this).children("div").html() == currentHour){
             //If the hours are the same then  present class gets added to textarea child element  to get the textbox color
             $(this).children("textarea").attr("class","col-8 col-lg-8 col-md-8 col-sm-8 textarea present");
         }
-    });
-}
-//Create function to add future class to timeblock if current time < timeblock hour
-function checkFuture(){
-    $(".timeblock").each(function(){
-        //tempDivHour takes the value of timeblock
-        var tempDivHour = $(this).children("div").html();
-        var tempDivPMorAM = "";
-        var tempCurrentPMorAM = "";
-        var tempHour= currentHour;
-        /*Sloppy method below will fix with a better method, it's working but not effecient */
-        /**Assigning PM or AM values taken from div child's timeblock hour.
-        ///When timeblock's string is 3 characters long i.e 9AM,5PM,...--*/
-        if(tempDivHour.length == 3){
-            tempDivPMorAM = tempDivHour[1]+tempDivHour[2];
-            tempDivHour = tempDivHour[0];
-        }
-        //If timeblocks string is not 3, then it must be 4
-        else {
-            tempDivPMorAM = tempDivHour[2]+tempDivHour[3];
-            tempDivHour = tempDivHour[0]+tempDivHour[1];
-        }
-        //When current time's string is 3 characters long i.e 9AM,5PM,...
-        if(tempHour.length == 3){
-            tempCurrentPMorAM = tempHour[1]+tempHour[2];
-            tempHour = tempHour[0];
-        }
-        //If current time's string is not 3, then it must be 4
-        else {
-            tempCurrentPMorAM = tempHour[2]+tempHour[3];
-            tempHour = tempHour[0]+tempHour[1];
-        }
-        /*From this point tempHour and tempDivHour are 1 or 2 digits
-        //and their PMorAM variables contain the PM or AM value------------*/
-        //Turning our number strings into integers to compare
-        tempDivHour = parseInt(tempDivHour);
-        tempHour = parseInt(tempHour);
+    /*FUTURE */
         //If timeblock hour is greater than current hour and they have same type of PM or AM
         /*We have to ignore timeblock 12PM because if current time is 1PM, then 12PM is larger but isn't logical */
         if(tempDivHour > tempHour && tempDivPMorAM == tempCurrentPMorAM  && tempDivHour !== 12){
@@ -150,6 +106,7 @@ function checkFuture(){
         }
     });
 };
+
 //When user clicks the save button, we save their message
 //Add an on click for every button clicked
 $(".saveBtn").on("click",function(){
@@ -172,7 +129,8 @@ function updateMessages(){
             //Since the timeblock is the parent of the actual div that houses our value this is how we call it
             //and compare it to our timeblock that was in our object
             if($(this).children("div").text() === timeblock)             
-                $(this).children("textarea").attr("placeholder", message);
+                //If the timeblock matches up with the message's timeblock then write the message
+                $(this).children("textarea").html(message);
         });
     }
 };
@@ -182,4 +140,8 @@ $("#clear").on("click", function(){
     messageArray = [];
     //We must pass the string "[]" to simulate an empty array
     localStorage.setItem("messages","[]");
+    //To clear every text field we go through each and replace the text with a space
+    $("textarea").each(function(){
+        $(this).html(" ");
+    });
 });
